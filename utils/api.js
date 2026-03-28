2c0d58ef6e26a78ab7547a15fc9bda1686dfc5db
Amit Yadav <yadavamit0186@gmail.com>
2026-03-29 00:58:13 +0530

    update

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "https://gaming-platform-backend-wp7e.onrender.com/api",
+    baseURL: import.meta.env.VITE_API_URL || "/api",
 });
 
 export default API;
